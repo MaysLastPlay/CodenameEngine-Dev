ebd10da259845647b8bdff1c022e8412b8f229ae
Furo <furogaming9@gmail.com>
2025-07-15 19:50:56 +0200

    Make game bootup?

--- source/funkin/backend/assets/ModsFolder.hx
+++ source/funkin/backend/assets/ModsFolder.hx
@@ -47,6 +47,8 @@ class ModsFolder {
 	 * Initializes `mods` folder.
 	 */
 	public static function init() {
+		if (!FileSystem.exists(modsPath)) FileSystem.createDirectory(modsPath);
+		if (!FileSystem.exists(addonsPath)) FileSystem.createDirectory(addonsPath);
 		if(!getModsList().contains(Options.lastLoadedMod))
 			Options.lastLoadedMod = null;
 	}
--- source/funkin/options/Options.hx
+++ source/funkin/options/Options.hx
@@ -38,7 +38,7 @@ class Options
 	public static var splashesEnabled:Bool = true;
 	public static var hitWindow:Float = 250;
 	public static var songOffset:Float = 0;
-	public static var framerate:Int = 120;
+	public static var framerate:Int = #if mobile 60 #else 120 #end;
 	public static var gpuOnlyBitmaps:Bool = #if (mac || web) false #else true #end; // causes issues on mac and web
 
 	public static var lastLoadedMod:String = null;
